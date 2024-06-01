9c8f76fdade31f7028fbaac9f0d7d9fecb9d0a0e
Aparna venugopal <aparnavenugopal1996@gmail.com>
2024-06-01 13:22:13 +0530

    next

--- basics/app.js
+++ basics/app.js
@@ -4,6 +4,15 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('in the middleware');
+    next();//allows the request to continue to the next imiddleware in line
+});
+app.use((req, res, next) => {
+    console.log('in the middleware2');
+    next();
+});
+
 const server = http.createServer();
 
 server.listen(3000);
