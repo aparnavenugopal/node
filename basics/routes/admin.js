f5f21766efe5182e1436e515e351f8e552051c6c
Aparna venugopal <aparnavenugopal1996@gmail.com>
2024-06-05 17:10:37 +0530

    templating done

--- basics/routes/admin.js
+++ basics/routes/admin.js
@@ -1,5 +1,6 @@
-const express = require('express');
 const path = require('path');
+
+const express = require('express');
 
 const rootDir = require('../util/path');
 
@@ -7,15 +8,20 @@ const router = express.Router();
 
 const products = [];
 
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
 });
 
+// /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-  console.log(req.body);
-  products.push({
-    title : req.body.title
-  })
+  products.push({ title: req.body.title });
   res.redirect('/');
 });
 
