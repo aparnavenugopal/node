d2cc4ebc44a104a9a9a8925a76eadf12f90a68ff
Aparna venugopal <aparnavenugopal1996@gmail.com>
2024-06-17 15:00:41 +0530

    sequalize syncing

--- basics/models/product.js
+++ basics/models/product.js
@@ -1,32 +1,75 @@
-const Cart = require('./cart');
-const db = require('../util/database');
+const Sequelize = require('sequelize');
 
+const sequelize = require('../util/database');
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, DESCRIPTION, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.DESCRIPTION = DESCRIPTION;
-    this.price = price;
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageUrl:{
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
   }
+});
 
-  save() {
-    return db.execute('INSERT INTO neproductsw_table (title, price, DESCRIPTION, imageUrl) VALUES(?, ?, ?, ?)',
-      [this.title, this.price, this.DESCRIPTION, this.imageUrl]
-    );
+module.exports = Product;
 
-  }
 
-  static deleteById(id) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const Cart = require('./cart');
+// const db = require('../util/database');
+
+
+// module.exports = class Product {
+//   constructor(id, title, imageUrl, DESCRIPTION, price) {
+//     this.id = id;
+//     this.title = title;
+//     this.imageUrl = imageUrl;
+//     this.DESCRIPTION = DESCRIPTION;
+//     this.price = price;
+//   }
+
+//   save() {
+//     return db.execute('INSERT INTO neproductsw_table (title, price, DESCRIPTION, imageUrl) VALUES(?, ?, ?, ?)',
+//       [this.title, this.price, this.DESCRIPTION, this.imageUrl]
+//     );
+
+//   }
+
+//   static deleteById(id) {
    
-  }
+//   }
 
-  static fetchAll() {
-    return db.execute('SELECT * FROM neproductsw_table');
-  }
+//   static fetchAll() {
+//     return db.execute('SELECT * FROM neproductsw_table');
+//   }
 
-  static findById(id, cb) {
-   
-  }
-};
+//   static findById(id) {
+//     return db.execute('SELECT * FROM neproductsw_table WHERE id = ?', [id]);
+//   }
+// };
--- basics/util/database.js
+++ basics/util/database.js
@@ -1,10 +1,32 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize('node-complete', 'root', 'Targetcat430#',{
+    dialect: 'mysql',
     host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'Targetcat430#'
-});
+} );
 
-module.exports = pool.promise();
+module.exports = sequelize;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const mysql = require('mysql2');
+
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'node-complete',
+//     password: 'Targetcat430#'
+// });
+
+// module.exports = pool.promise();
