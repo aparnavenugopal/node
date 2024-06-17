5191bf98ceaa7046bc4fcc2e87adf51458bbf7d0
Aparna venugopal <aparnavenugopal1996@gmail.com>
2024-06-17 11:53:08 +0530

    db connection success

--- basics/app.js
+++ basics/app.js
@@ -14,9 +14,9 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-db.execute('SELECT * FROM neproductsw_table').then(() => {}).catch((err) => {
-    console.log(err);
-});
+// db.execute('SELECT * FROM neproductsw_table').then(() => {}).catch((err) => {
+//     console.log(err);
+// });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
