293da8e0b59ff2d1cce8d0812250febc8e7b975f
Aparna venugopal <aparnavenugopal1996@gmail.com>
2024-06-17 13:20:11 +0530

    posting data into database

--- basics/models/product.js
+++ basics/models/product.js
@@ -3,15 +3,18 @@ const db = require('../util/database');
 
 
 module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, DESCRIPTION, price) {
     this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
-    this.description = description;
+    this.DESCRIPTION = DESCRIPTION;
     this.price = price;
   }
 
   save() {
+    return db.execute('INSERT INTO neproductsw_table (title, price, DESCRIPTION, imageUrl) VALUES(?, ?, ?, ?)',
+      [this.title, this.price, this.DESCRIPTION, this.imageUrl]
+    );
 
   }
 
